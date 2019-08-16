db16563d105e30827cf32a36b73907b07c173565
coderguanmingyang <1656499080@qq.com>
2019-08-16 21:44:38 +0800

    Add files via upload

---
+++ BinaryTree/230-Kth-Smallest-Element-in-a-BST.py
@@ -0,0 +1,79 @@
+# -*- coding:utf-8 -*-
+'''
+给定一个二叉搜索树，编写一个函数 kthSmallest 来查找其中第 k 个最小的元素。
+
+说明：
+你可以假设 k 总是有效的，1 ≤ k ≤ 二叉搜索树元素个数。
+
+示例 1:
+
+输入: root = [3,1,4,null,2], k = 1
+   3
+  / \
+ 1   4
+  \
+   2
+输出: 1
+示例 2:
+
+输入: root = [5,3,6,2,4,null,null,1], k = 3
+       5
+      / \
+     3   6
+    / \
+   2   4
+  /
+ 1
+输出: 3
+进阶：
+如果二叉搜索树经常被修改（插入/删除操作）并且你需要频繁地查找第 k 小的值，
+你将如何优化 kthSmallest 函数？
+
+'''
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+# 递归
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+
+        def mid_order(root):
+            if not root: return
+            mid_order(root.left)
+            res.append(root.val)
+            mid_order(root.right)
+
+        res = []
+        mid_order(root)
+        return res[k-1]
+
+## 迭代
+class Solutionv2(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        stack = []
+
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if not k:
+                return root.val
+            root = root.right
+
